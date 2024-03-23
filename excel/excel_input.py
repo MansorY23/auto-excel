6d190c2da5191e9a06ba475fae3cf60444a5934f
MansorY23 <arsa20034@mail.ru>
2024-03-23 09:55:44 +0300

    feat: correct group by dict and check final sum

--- excel/excel_input.py
+++ excel/excel_input.py
@@ -1,21 +1,36 @@
 import pandas as pd
-from categories import categories
+from .categories import categories
 from pathlib import Path
 from typing import Union
-
+import logging
 
 def excel_process(
         path: Union[Path, str]) -> pd.DataFrame:
 
     df = pd.read_excel(io=path, engine="calamine", header=None,
-                       skiprows=11, usecols=[0, 3, 6 ],
-                       names=["item", "number", "volume"], index_col=False,
+                       skiprows=10, usecols=[0, 3, 6 ],
+                       names=["item", "number", "volume"]
                        )
+    all_fuel = df["number"].loc[0]
+    print(all_fuel)
+    # регулярка, чтобы забирать только складские номера машин
     df = df[~df["item"].str.match("(\d{2}).(\d{2}).(\d{4}) (\d{1,2}):(\d{2}):(\d{2})")]
     df = df[df["item"].str.match("\d{5} ")]
-    df["item"] = [i[:3] for i in df["item"]]
-    df = df.groupby("item").sum("number")
-    print(df)
+
+    # добавляю индекс, т.к excel приходит кривой
+    df["index_column"] = range(1, len(df)+1)
+    df = df.set_index("index_column")
+
+    df["item"] = [i[:5] for i in df["item"]]
+    df = df.set_index("item").groupby(categories).sum()
+
+    # переменная для сравнения сходятся ли данные
+    all_fuel_after_agg = df["number"].sum()
+
+    if all_fuel != all_fuel_after_agg:
+        logging.error("Значения не сходятся. Возможно появился новый транспорт")
+        raise ValueError("Значения не сходятся. Возможно появился новый транспорт")
+    #print(df)
     return df
 
 
