e984d9896754f206552e6804fc0e684145448bdc
MansorY23 <arsa20034@mail.ru>
2024-04-11 21:34:43 +0300

    feat: main file add functions

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from excel import excel_process, update_excel
+from excel import (update_petrol,
+                   update_diesel,
+                   process_diesel,
+                   process_petrol)
 from outlook import save_attachments
 from dotenv import load_dotenv
-from services import daily_excel_name, monthly_excel_name
+from services import (daily_excel_name,
+                      monthly_excel_name)
 import os
 import logging
 
@@ -10,12 +14,14 @@ def main():
     logging.basicConfig(level=logging.INFO)
     load_dotenv()
     daily_excel_path = save_attachments(os.environ["SUBJECT"],
-                                        daily_excel_name(os.environ['EXCEL_PATH']),
+                        daily_excel_name(f"C:/Users/{os.getenv('username')}/Desktop/{os.environ['EXCEL_PATH']}"),
                                         ".xls", os.environ["INBOX_ITEM"])
 
-    daily_excel_df = excel_process(daily_excel_path)
-    update_excel(daily_excel_df,
-                 monthly_excel_name(os.environ["EXCEL_PATH"]))
+    monthly_excel = monthly_excel_name(daily_excel_path)
+    daily_excel = daily_excel_name(daily_excel_path)
+
+    update_diesel(process_diesel(daily_excel), monthly_excel)
+    update_petrol(process_petrol(daily_excel), monthly_excel)
 
     return None
 
